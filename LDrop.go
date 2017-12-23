56f839fcfc9f8cd975bb9bca2439b66923f35805
kamil <kamilkabir9@gmail.com>
2017-12-24 02:24:14 +0400

    create upload folder

--- LDrop.go
+++ LDrop.go
@@ -23,7 +23,6 @@ import (
 	"time"
 )
 
-//TODO add flag ignore hidden Folder flag
 const (
 	SuccessStatus = "Ok"
 	FailedStatus  = "Err"
@@ -156,6 +155,14 @@ func main() {
 	uploadFolder, err := filepath.Abs(uploadFolder)
 	if err != nil {
 		log.Panicln(err)
+	}
+	if uploadFolder=="Uploads"{
+		if _, err := os.Stat("Uploads"); os.IsNotExist(err) {
+			err=os.Mkdir("Uploads",0777)
+			if err!=nil{
+				log.Panicln(err)
+			}
+	}
 	}
 	statikFS, err = fs.New()
 	if err != nil {
